bbebfe450954c5d4156e257c6baad245b5199f13
Johann Weiher <info@suplanus.de>
2025-07-22 15:14:43 +0200

    Working example

--- test/Serilog.Sinks.Grafana.Loki.Tests/PlatformTests/WindowsTests.cs
+++ test/Serilog.Sinks.Grafana.Loki.Tests/PlatformTests/WindowsTests.cs
@@ -2,20 +2,18 @@
 using Serilog.Sinks.Grafana.Loki.Tests.TestHelpers;
 using Shouldly;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Serilog.Sinks.Grafana.Loki.Tests.PlatformTests;
 
 public class WindowsTests
 {
-  private const string ApprovalsFolderName = "Approvals";
-  private const string ExceptionStackTraceRegEx = @"(?<= in)(.*?)(?=},\\)";
-  private const string ExceptionStackTraceReplacement = " <stack-trace>";
-  private const string TimeStampRegEx = "\"[0-9]{19}\"";
-  private const string TimeStampReplacement = "\"<unixepochinnanoseconds>\"";
+  private readonly ITestOutputHelper _testOutputHelper;
   private readonly TestLokiHttpClient _client;
 
-  public WindowsTests()
+  public WindowsTests(ITestOutputHelper testOutputHelper)
   {
+    _testOutputHelper = testOutputHelper;
     _client = new TestLokiHttpClient();
   }
 
@@ -25,20 +23,17 @@ public class WindowsTests
     var logger = new LoggerConfiguration()
                  .WriteTo.GrafanaLoki(
                    "http://10.10.0.11:3100",
+                   propertiesAsLabels: new[] { "app", "environment" },
+                   labels: new[]
+                   {
+                     new LokiLabel { Key = "app", Value = "MD3" },
+                     new LokiLabel { Key = "environment", Value = "Test" }
+                   },
+                   textFormatter: new LokiJsonTextFormatter(),
                    httpClient: _client)
                  .CreateLogger();
 
-    logger.Information("This is an information without params");
+    logger.Information("WORKS!!!");
     logger.Dispose();
-    _client.Content.ShouldMatchApproved(
-      c =>
-      {
-        c.SubFolder(ApprovalsFolderName);
-        c.WithScrubber(
-          s => Regex.Replace(
-            s,
-            TimeStampRegEx,
-            TimeStampReplacement));
-      });
   }
 }
